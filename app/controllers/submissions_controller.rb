e0097980f166063d5e680875fbb718776adae582
Tushar Bhargava <tushar_bhargava@brown.edu>
2015-10-16 21:19:16 -0400

    Image functionality using blobs. Working.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,9 @@
+require 'base64'
+
 class SubmissionsController < ApplicationController
   def save
-   File.open('test.png',"wb") do |file|
-   file.write(Base64.decode64(params[:text_area]))
-end
+      File.open("#{Rails.root}/images/file1.png", 'wb') do |f|
+      f.write(params[:image].read)
+    end
   end
 end
