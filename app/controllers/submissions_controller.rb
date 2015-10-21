b6d7c1b79cba1ef285125f99640bc7e474f69aeb
BigYuns <sungyun.chung94@gmail.com>
2015-10-21 19:49:47 -0400

    date, post work

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,29 @@
 require 'base64'
-
+require 'date'
 class SubmissionsController < ApplicationController
   def save
-      File.open("#{Rails.root}/images/file1.png", 'wb') do |f|
-      f.write(params[:image].read)
+  	  #directory_name = DateTime.now.strftime('%m/%d/%Y')
+  	  title = params[:user]
+
+  	  puts "here is the title" 
+  	  puts params
+  	  
+  	  puts "user"
+  	  puts params[:user]
+  	
+
+  	  date = Date.today.to_s
+
+
+  	  #@title = params[:user]
+
+  	  #@date = "date"
+  	  Dir.mkdir(date) unless File.exists?(date)
+
+      File.open("#{Rails.root}/#{date}/#{title}.png", 'wb') do |f|
+      	f.write(params[:image].read)
+
     end
   end
+
 end
